9308009b83a3092906e7663a8040aa5f33dc1342
BhavadharaniD <bhavadharani2027@gmail.com>
2025-12-27 16:20:19 +0530

    Update sidebar

--- frontend/vite-project/src/components/placement/Sidebar.jsx
+++ frontend/vite-project/src/components/placement/Sidebar.jsx
@@ -1,7 +1,16 @@
 import { Briefcase, PlusCircle } from "lucide-react";
 import { NavLink } from "react-router-dom";
-
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 export default function PlacementSidebar({ onCreateDrive }) {
+  const { logout } = useAuth();
+   const navigate = useNavigate();
+   
+  const handleLogout = () => {
+    logout();                   // clear auth
+    navigate("/", { replace: true }); // 🔥 PUBLIC HOME
+  };
+
   return (
     <aside className="w-64 min-h-screen bg-[#020617] text-slate-200 fixed">
       <div className="p-6 font-bold text-xl text-white">
@@ -24,6 +33,12 @@ export default function PlacementSidebar({ onCreateDrive }) {
   ➕ Create Drive
 </button>
 
+<button
+      onClick={handleLogout}
+      className="flex gap-3 items-center text-red-400 mt-8"
+    >
+      <handleLogout /> Logout
+    </button>
       </nav>
     </aside>
   );
